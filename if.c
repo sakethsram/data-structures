57dcf75e5cc14553a7e0f47453ffe92a99231f7a
bhagavan <bhagavan@auranetworks.in>
2021-07-12 22:54:01 -0400

    nested if

---
+++ if.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+	{
+		int m , p,c,j=35;
+		m=35;
+		p=35;
+		c=35;
+		if(m>=35)
+		{
+			if(p>=35)
+			{
+				if(c>=35)
+				{
+					printf("promoted");
+					printf("\n");
+	
+				}
+			}	
+		}
+		else
+		{	
+			printf(" not promoted");
+			printf("\n");
+		}
+	}	
+
+
