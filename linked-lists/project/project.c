5ca498a4eedd0b27702365ac3d72ded28c37d60c
bhagavan <bhagavan@auranetworks.in>
2021-09-29 18:19:17 +0530

    After fixing bugs

--- linked-lists/project/project.c
+++ linked-lists/project/project.c
@@ -1,52 +1,69 @@
 #include "stdio.h"
 #include "string.h"
+
+#define LANE_SIZE 8
+
 struct hash
 {
 	char lane[128];
 	int no;
 };
-struct hash h[32];
-int n=0;
-int cmp(char loc[32])
+
+struct hash map_table[LANE_SIZE];
+
+int get_lane_pos(int v, char *lane)
 {
 	int i=0;
-	while (i<=32)
+
+	for (i = 0; i <= LANE_SIZE; i++)
 	{
-		if(strcmp(h[i].lane,loc)==0)
+		if(strlen(map_table[i].lane)==0)
 			break;
-		if(strlen(h[i].lane)==0)
+
+		if(strcmp(map_table[i].lane, lane)== 0)
 			break;
-		i++;	
 	}
+
+	if (i < LANE_SIZE)
+	{
+		strcpy(map_table[i].lane, lane);
+		map_table[i].no = v;
+		return i;
+	}
+
+	return -1;
 }	
+
 int dumplist(void)
 {
 	int i=0;
-	for(i=0;i<=7;i++)
-	printf("\n  villa %d lane: %s \n",h[i].no,h[i].lane);
+
+	for(i=0; i<= LANE_SIZE;i++)
+	{
+		if (strlen(map_table[i].lane) == 0)
+			break;
+		printf("%d. %s, %d\n", i+1, map_table[i].lane, map_table[i].no);
+	}
 }
-int addnode(int v,char pos[32])
+
+int addnode(int v, char *lane)
 {
-	int  n;
-	n=cmp(pos);
-	strcpy(h[n].lane,pos);
-	h[n].no=v;
+	int n, i = 0;
+	n = get_lane_pos(v, lane);
 	return 0;
 }	
 int main()
 {
-	addnode(112,"2ll");
-	dumplist();
-	addnode(114,"2lR");
+	addnode(112, "2LL");
+	addnode(114, "2LR");
+	addnode(85,  "1LL");
+	addnode(86,  "2RL");
+	addnode(93,  "2LL");
+	addnode(103, "2LL");
+	addnode(89,  "2LL");
+	addnode(104, "2LL");
+	addnode(1,   "2LL");
+	addnode(112, "2LL");
 	dumplist();
 	return 0;
-	addnode(85,"1ll");
-	addnode(86,"2Rl");
-	addnode(93,"2ll");
-	addnode(103,"2ll");
-	addnode(89,"2ll");
-	addnode(104,"2ll");
-	addnode(1,"2ll");
-	addnode(112,"2ll");
-	dumplist();
 }	
