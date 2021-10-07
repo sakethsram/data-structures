b02c721404c029254af9ecdbcd0510398972f48f
bhagavan <bhagavan@auranetworks.in>
2021-10-07 20:50:15 +0530

    Adding Binary trees

---
+++ linked-lists/hash/project.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "string.h"
+struct hash_map
+{
+	char lane[128];
+	int no;
+}
+struct hash map[64];
+int t=0;
+int commpare(char lane)
+
+
--- linked-lists/trees/addnode.c
+++ linked-lists/trees/addnode.c
@@ -1,50 +1,64 @@
 #include "stdio.h"
 #include "stdlib.h"
+
 struct node 
 {
-	struct node *pre;
+	struct node *prev;
 	int v;
 	struct node *next;
 };	
+
+struct node *Head = NULL;
+
 int add_node(int val)
 {
-	struct node *t=NULL;
-	struct node *p=NULL;
-	struct node *h=NULL;
+	struct node *t = Head;
+	struct node *p = NULL;
+
 	p=malloc(sizeof(struct node));
 	p->v=val;
-	if(h==NULL)
+	p->next = NULL;
+	p->prev = NULL;
+
+	if(Head==NULL)
 	{
-		h=p;
-		p->pre=NULL;
-		p->next=NULL;
+		Head=p;
 		return 0;
 	}
-	while(t!=NULL)
+
+	while(t != NULL)
 	{
-		if((t->v)<val)
+		if(val > t->v)
 		{
 			if(t->next==NULL)
 			{
-				t->next=p;
+				t->next = p;
 				return 0;
 			}
-			if(t->next!=NULL)
-				t=t->next;
-		}
-		if((t->v)>val)	
-		{
-			if(t->pre==NULL)
-			{
 
-				t->pre=p;
+			t=t->next;
+		}
+
+		if(val < t->v)
+		{
+			if(t->prev==NULL)
+			{
+				t->prev=p;
 				return 0;
 			}
-			t=t->pre;
+
+			t = t->prev;
 		}	
 	}
-
 }
+
+int values[] = {8, 12, 10, 7, 32, 16, 14, 11};
 int main()
 {
+	int i = 0;
+
+	for (i = 0; i <= sizeof(values)/sizeof(int); i++)
+	{
+		add_node(values[i]);
+	}
 }
