eb0d3b0abfb5f8f217c100f3eda093953a7aede1
bhagavan <bhagavan@auranetworks.in>
2021-10-27 20:54:03 -0400

    adding all files

--- linked-lists/trees/bin_trees.c
+++ linked-lists/trees/bin_trees.c
@@ -1,15 +1,33 @@
 #include "stdio.h"
 #include "stdlib.h"
+#define MAX_STACK_SIZE 10
+int needle = 0;
+int stack[MAX_STACK_SIZE];
+
 
 struct node 
 {
 	struct node *prev;
 	int v;
 	struct node *next;
-};	
-
-struct node *Head = NULL;
-
+};
+int pop(void)
+{
+	int t;
+	t = stack[needle];
+	needle--;
+	return t;
+}
+int push(int v)
+{
+	if (needle > MAX_STACK_SIZE)
+	{
+		printf("Stack overflow\n");
+		return 0;
+	}
+	stack[needle] = v;
+	needle++;
+}
 int add_node(int val)
 {
 	struct node *t = Head;
@@ -39,6 +57,7 @@ int add_node(int val)
 			t=t->next;
 		}
 
+
 		if(val < t->v)
 		{
 			if(t->prev==NULL)
@@ -52,6 +71,19 @@ int add_node(int val)
 	}
 }
 
+void display_right(void)
+{
+	struct node *t=NULL;
+	struct node *p=NULL;
+	int i=0;
+
+	printf("\n");
+	for(i = 0, t = Head; i <=MAX_STACK_SIZE, t != NULL; t = t->next,i++)
+		stack[i] = t->v ;
+	printf("\n");
+}
+
+
 int values[] = {8, 12, 10, 7, 32, 16, 14, 11};
 int main()
 {
@@ -61,4 +93,5 @@ int main()
 	{
 		add_node(values[i]);
 	}
+	display_right();
 }
