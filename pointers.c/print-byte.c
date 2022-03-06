2e6c866c58a1bbf9a4942ae56c48a911232fa5fd
bhagavan <bhagavan@auranetworks.in>
2022-03-06 08:41:28 +0530

    adding files

--- pointers.c/print-byte.c
+++ pointers.c/print-byte.c
@@ -1,13 +1,11 @@
 #include "stdio.h"
 int main()
 {
-	int a = 300, i, *p;
+	int   a=300,i,*p;
+	p=&a;
 	char *pch;
-
-	p = &a;
-	pch = (char *)p;
-
-	for(i = 1; i <= 4; i++, pch++)
-		printf(" %d \n " , *pch);
+	pch=p;
+	for(i=1;i<=4;i++,pch++)
+		printf(" %u \n " , *pch);
 }
 
--- strings/astrncmp.c
+++ strings/astrncmp.c
@@ -12,15 +12,15 @@ int main()
 	char s2[]="SAKETH1";
 	for(i=0, j=0 ; s1[i]!='\0' && s2[i]!='\0' && i<=n-1 && j<=n-1 ; i++,j++)
 	{
-		 t1= astlower( s1[i] );
-		 t2= astlower( s2[i] );
-		 if( t1==t2)
-		  	temp++;
-		 else
-			 break;
+		t1= astlower( s1[i] );
+		t2= astlower( s2[i] );
+		if( t1==t2)
+			temp++;
+		else
+			break;
 	}
-		printf("%s \n",s1);
-		printf("%s \n",s2);
+	printf("%s \n",s1);
+	printf("%s \n",s2);
 	if(temp > 0)
 		printf(" same \n ");
 	else
