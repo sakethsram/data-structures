81b175e7913d44a55f2c610fa4d190f2acfc5278
bhagavan <bhagavan@auranetworks.in>
2022-03-30 17:51:39 +0530

    adding files

--- linked-lists/tree/binary_tree/psearch.c
+++ linked-lists/tree/binary_tree/psearch.c
@@ -5,6 +5,7 @@ struct node *parent=NULL;
 struct node *c=NULL;
 struct node* p_search(int val)
 {
+	struct node* flag=NULL;
 	parent = NULL;
 	c=root;
 	int i;
@@ -27,32 +28,57 @@ struct node* p_search(int val)
 	printf("Searching value %d Found :%d at %p\n", val, c->v, c);
 	printf("            parent value :%d at %p\n", parent->v, parent);
 	i=no_of_children(c);
-	if(i==0)
+	int bt_delete_leaf_node(struct node*c,struct node *parent)
 	{
 			if(c->right==NULL && c->left==NULL)
 			{
 				if(parent->right!=NULL)
-					parent->right=NULL;
+				{
+					if(parent->right->v==c)
+						parent->right=NULL;
+				}
 				if(parent->left!=NULL)
+				{
+					if(parent->left->v==c)
 					parent->left=NULL;
+				}	
 				free(c);
 				return 0;
 			}
 	}	
-	if(i==1)
-	{
-		if(c->left!=NULL && c->right==NULL)
+	int bt_delete_single_child_node(struct node*c,struct node *parent)
+	{	
+		if(c->right==NULL && c->left==NULL)
 		{
-			c->v = c->left->v;
-			free(c->left);
-			c->left = c->left->left; 
-		}	
-		if(c->left==NULL && c->right!=NULL)
-		{
-			c->v = c->right->v;
-			free(c->right);
-			c->right = c->right->right; 
+			if(parent->right!=NULL)
+				parent->right=NULL;
+			if(parent->left!=NULL)
+				parent->left=NULL;
+			free(c);
+			return 0;
 		}
-	}	
+	}
+	flag=get_min_right_node_by_root(c);
+	if(i==2)
+	{
+		flag=get_min_right_node_by_root(c);
+		c->v=flag;
+		flag2= is_leaf_node(c->right);
+		if(flag2==1)
+		{
+		}
+	}
+	nc = c->right;
+	nparent = NULL;
+	while(TRUE)
+	{
+		nparent=c;
+		if(val > c->v)
+			nc=nc->right;
+		if(val < nc->v)
+			nc=nc->left;
+		if(val == c->v)
+			break;
+	}
 }
-	
+
