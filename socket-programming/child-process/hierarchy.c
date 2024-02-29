694ccb2279b32bc0366825d0eb7b6e9df64661ab
saketh <sakethramsettipalli2003@gmail.com>
2024-02-29 17:33:29 +0530

    adding h and sibling at the same time

---
+++ socket-programming/child-process/hierarchy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+int main()
+{
+    int t, n=5,i;
+    for(i=0;i<n;i++)
+    {
+        t=fork();
+        if(t!=0)
+            break;            
+    }
+    sleep(100);
+   
+}
+
+
