5df3cbca3a987fbb0bc8e4cef2bb3bd00ffe267c
saketh <sakethramsettipalli2003@gmail.com>
2024-03-09 10:30:07 +0530

    changes were made

--- python/temp.py
+++
@@ -1,5 +0,0 @@
-str1 = "mount everest is the highest peak in the world, mount everest is present in himalayas"
-print(str1.count("mount", 0, 80))
-print(str1.find("peak"))
-print(str1.find("mount"))
-print(str1.find("everest"))
